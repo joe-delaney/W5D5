b06623dd2612002eb7a171e079c5731f11400686
App Academy Student <email@email.com>
2022-03-04 17:06:00 -0500

    finished migrations and associations for polls

---
+++ polls_app/app/models/poll.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: polls
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  author_id  :integer          not null
+#
+class Poll < ApplicationRecord
+  belongs_to :author,
+    primary_key: :id,
+    foreign_key: :author_id,
+    class_name: :User
+
+  has_many :questions,
+    primary_key: :id,
+    foreign_key: :poll_id,
+    class_name: :Question
+end
