024b517864e320e3139968715eec2e8ab0747ee5
App Academy Student <email@email.com>
2022-03-04 14:12:32 -0500

    finished queries 1

--- movie_buff_project/movie_buff/01_queries.rb
+++ movie_buff_project/movie_buff/01_queries.rb
@@ -69,7 +69,15 @@ def directed_by_one_of(them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
 
-
+  # Movie
+  #   .joins(:actors)
+  #   .select('movies.id,movies.title')
+  #   .where('actors.name in (?)',them)
+  Actor
+    .joins(:directed_movies)
+    .select('movies.id,movies.title')
+    .where('actors.name in (?)',them)
+    
 end
 
 def movie_names_before_1940
@@ -83,5 +91,7 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+
+  Movie.where('yr < 1940').pluck(:title)
 
 end
